43c0f346501a6db5a40a82e54635af84cc87fe07
Matt Lane <lane.matthew@gmail.com>
2016-11-11 12:39:32 -0800

    add ability to update x axis graph

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 var width = 500;
 var height = 500;
-var svg = d3.select("svg");
-svg.attr('width', width).attr('height', height);
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
 
-var timeMin = d3.min(movies, function(d) {
-  return d.release;
+var padding = {
+  top: 20,
+  right: 20,
+  bottom: 20,
+  left: 30
+};
+
+var tooltip = d3.select("body")
+                .append("div")
+                .attr("class", "tooltip")
+                .style("opacity",0)
+                .style("position","absolute");
+
+var xMin = d3.min(movies, function(d) {
+  return d.daysOpen;
 });
 
-var timeMax = d3.max(movies, function(d) {
-  return d.release;
+var xMax = d3.max(movies, function(d) {
+  return d.daysOpen;
 });
 
-var totalMin = d3.min(movies, function(d) {
+var yMin = d3.min(movies, function(d) {
   return d.total;
 });
 
-var totalMax = d3.max(movies, function(d) {
+var yMax = d3.max(movies, function(d) {
   return d.total;
 });
 
-var xScale = d3.scaleTime()
-               .domain([timeMin, timeMax])
-               .range([0,width]);
+var xScale = d3.scaleLinear()
+               .domain([xMin, xMax])
+               .range([padding.left,width - padding.right]);
 
 var yScale = d3.scaleLinear()
-               .domain([totalMin, totalMax])
-               .range([height,0]);
+               .domain([yMin, yMax])
+               .range([height - padding.top,padding.bottom]);
 
 var colorScale = d3.scaleLinear()
                    .domain([0,1])
@@ -37,7 +52,7 @@ svg.selectAll("circle")
   .enter()
   .append('circle')
   .attr('cx', function(d) {
-    return xScale(d.release);
+    return xScale(d.daysOpen);
   })
   .attr('cy', function(d) {
     return yScale(d.total);
@@ -47,19 +62,56 @@ svg.selectAll("circle")
   })
   .attr('fill', function(d) {
     return colorScale(d.freshness);
+  })
+  .attr('stroke', 'black')
+  .on("mouseenter", function(d) {
+    tooltip.text(d.title)
+           .style("opacity", 1)
+           .style("left", d3.event.pageX + "px")
+           .style("top", d3.event.pageY + "px");
+  })
+  .on("mouseout", function() {
+    tooltip.style("opacity", 0);
   });
 
-// add text
-svg.selectAll('text')
-  .data(movies)
-  .enter()
-  .append('text')
-  .text(function(d) {
-    return d.title;
-  })
-  .attr('x', function(d) {
-    return xScale(d.release);
-  })
-  .attr('y', function(d) {
-    return yScale(d.total);
+svg.append('g')
+   .attr("class","x-axis")
+   .attr("transform", "translate(0," + (height - padding.top) + ")")
+   .call(d3.axisBottom(xScale));
+
+svg.append('g')
+   .attr("transform", "translate(" + padding.left + ",0)")
+   .call(d3.axisLeft(yScale));
+
+d3.select("select").on("change", function() {
+  var scaleType;
+  newVal = d3.select("select").property("value");
+  xMin = d3.min(movies, function(d) {
+    return d[newVal];
   });
+  xMax = d3.max(movies, function(d) {
+    return d[newVal];
+  });
+  if (newVal === "release") {
+    scaleType = d3.scaleTime;
+  } else {
+    scaleType = d3.scaleLinear;
+  }
+  xScale = scaleType().domain([xMin,xMax])
+                    .range([padding.left,width - padding.right]);
+ 
+  var t = d3.transition()
+                     .duration(1000)
+                     .ease(d3.easeBounceOut);
+
+  d3.selectAll('circle')
+    .transition(t)
+    .attr('cx', function(d) {
+      return xScale(d[newVal]);
+    });
+  
+  d3.selectAll('.x-axis')
+    .transition(t)
+    .call(d3.axisBottom(xScale));
+
+});
--- movies.js
+++ movies.js
@@ -3,8 +3,8 @@ var movies = [{
   release: new Date("June 17 2016"),
   rating: "PG",
   runtime: 103,
-  total: 485501100,
-  openingTotal: 135060273,
+  total: 485501100/1e6,
+  openingTotal: 135060273/1e6,
   maxTheaters: 4305,
   daysOpen: 130,
   freshness: 0.94
@@ -13,8 +13,8 @@ var movies = [{
   release: new Date("May 6 2016"),
   rating: "PG-13",
   runtime: 147,
-  total: 408084349,
-  openingTotal: 179139142,
+  total: 408084349/1e6,
+  openingTotal: 179139142/1e6,
   maxTheaters: 4226,
   daysOpen: 140,
   freshness: 0.90
@@ -23,8 +23,8 @@ var movies = [{
   release: new Date("July 8 2016"),
   rating: "PG",
   runtime: 90,
-  total: 366201540,
-  openingTotal: 104352905,
+  total: 366201540/1e6,
+  openingTotal: 104352905/1e6,
   maxTheaters: 4381,
   daysOpen: 109,
   freshness: 0.75
@@ -33,8 +33,8 @@ var movies = [{
   release: new Date("August 5 2016"),
   rating: "PG-13",
   runtime: 130,
-  total: 324302274,
-  openingTotal: 133682248,
+  total: 324302274/1e6,
+  openingTotal: 133682248/1e6,
   maxTheaters: 4255,
   daysOpen: 81,
   freshness: 0.26
@@ -43,8 +43,8 @@ var movies = [{
   release: new Date("July 29 2016"),
   rating: "PG-13",
   runtime: 123,
-  total: 162169190,
-  openingTotal: 59215365,
+  total: 162169190/1e6,
+  openingTotal: 59215365/1e6,
   maxTheaters: 4039,
   daysOpen: 88,
   freshness: 0.56
@@ -53,8 +53,8 @@ var movies = [{
   release: new Date("July 22 2016"),
   rating: "PG-13",
   runtime: 120,
-  total: 158848340,
-  openingTotal: 59253211,
+  total: 158848340/1e6,
+  openingTotal: 59253211/1e6,
   maxTheaters: 3928,
   daysOpen: 91,
   freshness: 0.84
@@ -63,8 +63,8 @@ var movies = [{
   release: new Date("May 27 2016"),
   rating: "PG-13",
   runtime: 143,
-  total: 155442489,
-  openingTotal: 65769562,
+  total: 155442489/1e6,
+  openingTotal: 65769562/1e6,
   maxTheaters: 4153,
   daysOpen: 63,
   freshness: 0.48
@@ -73,8 +73,8 @@ var movies = [{
   release: new Date("July 15 2016"),
   rating: "PG-13",
   runtime: 108,
-  total: 128254492,
-  openingTotal: 46018755,
+  total: 128254492/1e6,
+  openingTotal: 46018755/1e6,
   maxTheaters: 3963,
   daysOpen: 101,
   freshness: 0.72
@@ -83,8 +83,8 @@ var movies = [{
   release: new Date("June 17 2016"),
   rating: "PG-13",
   runtime: 107,
-  total: 127440871,
-  openingTotal: 35535250,
+  total: 127440871/1e6,
+  openingTotal: 35535250/1e6,
   maxTheaters: 3508,
   daysOpen: 77,
   freshness: 0.69
@@ -93,8 +93,8 @@ var movies = [{
   release: new Date("July 1 2016"),
   rating: "PG-13",
   runtime: 109,
-  total: 126643061,
-  openingTotal: 38527856,
+  total: 126643061/1e6,
+  openingTotal: 38527856/1e6,
   maxTheaters: 3591,
   daysOpen: 77,
   freshness: 0.36
@@ -103,8 +103,8 @@ var movies = [{
   release: new Date("July 29 2016"),
   rating: "R",
   runtime: 101,
-  total: 113226532,
-  openingTotal: 23817340,
+  total: 113226532/1e6,
+  openingTotal: 23817340/1e6,
   maxTheaters: 3215,
   daysOpen: 88,
   freshness: 0.60
@@ -113,8 +113,8 @@ var movies = [{
   release: new Date("May 20 2016"),
   rating: "PG",
   runtime: 97,
-  total: 107509366,
-  openingTotal: 38155177,
+  total: 107509366/1e6,
+  openingTotal: 38155177/1e6,
   maxTheaters: 3932,
   daysOpen: 119,
   freshness: 0.43
@@ -123,8 +123,8 @@ var movies = [{
   release: new Date("June 24 2016"),
   rating: "PG-13",
   runtime: 120,
-  total: 103144286,
-  openingTotal: 41039944,
+  total: 103144286/1e6,
+  openingTotal: 41039944/1e6,
   maxTheaters: 4130,
   daysOpen: 84,
   freshness: 0.31
@@ -133,8 +133,8 @@ var movies = [{
   release: new Date("June 10 2016"),
   rating: "R",
   runtime: 134,
-  total: 102470008,
-  openingTotal: 40406314,
+  total: 102470008/1e6,
+  openingTotal: 40406314/1e6,
   maxTheaters: 3356,
   daysOpen: 77,
   freshness: 0.80
@@ -143,8 +143,8 @@ var movies = [{
   release: new Date("August 12 2016"),
   rating: "R",
   runtime: 89,
-  total: 97636799,
-  openingTotal: 34263534,
+  total: 97636799/1e6,
+  openingTotal: 34263534/1e6,
   maxTheaters: 3135,
   daysOpen: 73,
   freshness: 0.83
@@ -153,8 +153,8 @@ var movies = [{
   release: new Date("August 26 2016"),
   rating: "R",
   runtime: 88,
-  total: 88577905,
-  openingTotal: 26411706,
+  total: 88577905/1e6,
+  openingTotal: 26411706/1e6,
   maxTheaters: 3384,
   daysOpen: 59,
   freshness: 0.87
@@ -163,8 +163,8 @@ var movies = [{
   release: new Date("June 3 2016"),
   rating: "PG-13",
   runtime: 112,
-  total: 82051601,
-  openingTotal: 35316382,
+  total: 82051601/1e6,
+  openingTotal: 35316382/1e6,
   maxTheaters: 4071,
   daysOpen: 98,
   freshness: 0.38
@@ -173,8 +173,8 @@ var movies = [{
   release: new Date("July 1 2016"),
   rating: "R",
   runtime: 105,
-  total: 79042440,
-  openingTotal: 31515110,
+  total: 79042440/1e6,
+  openingTotal: 31515110/1e6,
   maxTheaters: 2821,
   daysOpen: 49,
   freshness: 0.55
@@ -183,8 +183,8 @@ var movies = [{
   release: new Date("May 27 2016"),
   rating: "PG",
   runtime: 112,
-  total: 77041381,
-  openingTotal: 26858726,
+  total: 77041381/1e6,
+  openingTotal: 26858726/1e6,
   maxTheaters: 3763,
   daysOpen: 98,
   freshness: 0.30
@@ -193,9 +193,11 @@ var movies = [{
   release: new Date("August 12 2016"),
   rating: "PG",
   runtime: 102,
-  total: 75335573,
-  openingTotal: 21514095,
+  total: 75335573/1e6,
+  openingTotal: 21514095/1e6,
   maxTheaters: 3702,
   daysOpen: 74,
   freshness: 0.86
-}];
+}].sort(function(m1,m2) {
+  return m2.total / m2.openingTotal - m1.total / m1.openingTotal;
+});
